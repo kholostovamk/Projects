c28c3627aa7ae2d05e347712695ea63a8130729b
Margo <kholostova@gmail.com>
2023-10-29 16:54:32 -0400

    stated idea for further improvements

--- webPars.py
+++ webPars.py
@@ -14,42 +14,50 @@ import pandas as pd
 
 import pprint
 
-#request
-URL_Template = "http://books.toscrape.com/?" 
+# request
+URL_Template = "http://books.toscrape.com/?"
 request1 = requests.get(URL_Template)
-#print(request1.status_code)
+# print(request1.status_code)
 
-#print(request1.text)
+# print(request1.text)
 
-#want to find out books names
+# want to find out books names
 
 soup = bs(request1.text, "html.parser")
 
-#find all article elements with the class 'product_pod'
-articles = soup.find_all('article', class_ = 'product_pod')
+# find all article elements with the class 'product_pod'
+articles = soup.find_all('article', class_='product_pod')
 
 print(len(articles))
 
-#dictionary to store books info
+# dictionary to store books info
 books_dict = {}
 
 for article in articles:
-   title =  article.img['alt']
-   price_tag = article.find('p', class_ = 'price_color')
-   price = price_tag.text if price_tag else None
-   rating = article.find('p', class_ = 'star-rating')['class'][1] #actual rating, not stars
-   availability = article.find('p', class_ = "instock availability").text.strip()
-   
-   #storing it in dictionary
-   books_dict[title] = {
-      'Price': price,
-      'Rating': rating,
-      'Availability': availability
+    title = article.img['alt']
+    price_tag = article.find('p', class_='price_color')
+    price = price_tag.text if price_tag else None
+    # actual rating, not stars
+    rating = article.find('p', class_='star-rating')['class'][1]
+    availability = article.find(
+        'p', class_="instock availability").text.strip()
+
+    # storing it in dictionary
+    books_dict[title] = {
+        'Price': price,
+        'Rating': rating,
+        'Availability': availability
     }
 
 
-#printing out dictionary
+# printing out dictionary
 for title, details in books_dict.items():
-   print(f'{title}')
-   for key, value in details.items():
-      print(f'   {key}: {value}')
+    print(f'{title}')
+    for key, value in details.items():
+        print(f'   {key}: {value}')
+
+"""to improve:
+1. error handling
+2. make into separate functions
+3. save to file instead of dictionary
+4. """
