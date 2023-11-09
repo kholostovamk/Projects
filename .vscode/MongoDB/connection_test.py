a66f96fc91a5c9bdb1d824a989f2a8562b76cac2
Margo <kholostova@gmail.com>
2023-11-09 09:58:29 -0500

    added new document

--- .vscode/MongoDB/connection_test.py
+++ .vscode/MongoDB/connection_test.py
@@ -1,6 +1,7 @@
 from xml.dom.xmlbuilder import DocumentLS
 import pymongo
 from tomlkit import document
+import pprint
 
 client = pymongo.MongoClient("localhost", 27017)
 
@@ -8,12 +9,12 @@ db = client['my_db']
 
 ##creating collection
 
-collection = db['Northeastern_Database']
+collection_name = db['Northeastern_Database']
 
 # Check if the collection exists
-if collection in db.list_collection_names():
+if collection_name in db.list_collection_names():
     # Drop the collection if it exists
-    db[collection].drop()
+    db[collection_name].drop()
 
 documents = [ {'Students':[ {
     'id': 1,
@@ -41,7 +42,7 @@ documents = [ {'Students':[ {
     }]}
 ]
 
-insert_result = collection.insert_many(documents)
+insert_result = collection_name.insert_many(documents)
 
 print(f'Inserted {len(insert_result.inserted_ids)} documents')
 
@@ -49,16 +50,46 @@ print(f'Inserted {len(insert_result.inserted_ids)} documents')
 
 filter = {'Employees.name': 'John Adams'}
 
-cursor = collection.find(filter)
+cursor = collection_name.find(filter)
 
-update = [ { '$set': {
+update = {  '$set': {
     'Employees.$.name': "Johny Adams Jr."
-}}]
+}}
 
-result = collection.update_one(filter, update)
+result = collection_name.update_one(filter, update)
 
 if result.modified_count > 0:
-    print("Update succeeful")
+    print("Update successful")
 else:
     print('No document matched the filter')
 
+
+
+#adding a name to the collection
+
+new_student = {
+    'id': 3,
+    'name': 'William More',
+    'email': 'w,more@northeastern.edu',
+    'year': 'Freshman',
+    'major': 'Biology'
+}
+
+filter = {}
+
+update = {'$push': {'Students': new_student}}
+
+result2 = collection_name.update_one(filter, update)
+
+if result2.modified_count > 0:
+    print("Update successful")
+else:
+    print('No document matched the filter')
+
+#printing out collection
+pp = pprint.PrettyPrinter(indent=4)
+
+cursor = collection_name.find({})
+
+for document in cursor:
+   pp.pprint(document)
